b3a4d8c25623b240d57066f15379806a28b7f682
himajin-3 <kengokamiguchi@gmail.com>
2024-09-08 16:05:06 +0000

    test

--- study/codepipeline.tf
+++ study/codepipeline.tf
@@ -1,16 +1,46 @@
-resource "aws_lambda_function" "my_lambda" {
-  function_name = "test"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"  # Lambdaのエントリーポイント
-  runtime       = "python3.9
+resource "aws_codepipeline" "my_pipeline" {
+  name     = "MyCodePipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
 
-  filename         = "test"  # ローカルファイル（LambdaコードのZipファイル）
-  # source_code_hash = filebase64sha256("lambda_function_payload.zip")  # ファイルのハッシュで変更を検知
+  artifact_store {
+    location = "codepipeline-study-kamiguchi"
+    type     = "S3"
+  }
 
-  environment {
-    variables = {
-      ENV_VAR_1 = "value1"
-      ENV_VAR_2 = "value2"
+  depends_on = ["codepipeline-study-kamiguchi"]
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "S3"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        S3Bucket    = "codepipeline-study-kamiguchi"
+        S3ObjectKey = "source.zip"
+      }
+    }
+  }
+
+  stage {
+    name = "InvokeLambda"
+
+    action {
+      name             = "InvokeLambda"
+      category         = "Invoke"
+      owner            = "AWS"
+      provider         = "Lambda"
+      input_artifacts  = ["source_output"]
+      version          = "1"
+
+      configuration = {
+        FunctionName = aws_lambda_function.my_lambda.function_name
+      }
     }
   }
 }
